9728e51bc91bd49c8ef0fb77a0ca367926541f55
vishwas <vishwasj88@gmail.com>
2022-11-22 18:13:46 +0100

    DH parameter verified

--- misc/DH_param_transformation.m
+++ misc/DH_param_transformation.m
@@ -9,6 +9,10 @@ T_33d = transformation_matrix(d3, 0, pi, 124);
 T_3d4 = transformation_matrix(-pi/2, 0, -pi/2, 0);
 T_45 = transformation_matrix(d4, 167, pi/2, 0);
 
+T_55d = transformation_matrix(d5, 0, -pi/2, 0);
+T_5d6 = transformation_matrix(0, 104, 0, 112);
+
+
 T = T_01*T_12*T_23*T_33d*T_3d4*T_45; 
 
 
@@ -33,10 +37,12 @@ r1  = T_01(:,4);
 r2  = T_01*T_12(:,4);
 r3  = T_01*T_12*T_23(:,4);
 r3d = T_01*T_12*T_23*T_33d(:,4);
-r4  = T_01*T_12*T_23*T_3d4(:,4);
+r4  = T_01*T_12*T_23*T_33d*T_3d4(:,4);
 r5  = T_01*T_12*T_23*T_33d*T_3d4*T_45(:,4);
+r5d = T_01*T_12*T_23*T_33d*T_3d4*T_45*T_55d(:,4);
+r6  = T_01*T_12*T_23*T_33d*T_3d4*T_45*T_55d*T_5d6(:,4);
 
-r = r5;
+r = r6;
 
 %% DEBUG PURPOSE
 vr1 = vpa(subs(r1, {d1,d2,d3,d4,d5}, {0,0,0,0,0}));
@@ -45,5 +51,7 @@ vr3 = vpa(subs(r3, {d1,d2,d3,d4,d5}, {0,0,0,0,0}));
 vr3d = vpa(subs(r3d, {d1,d2,d3,d4,d5}, {0,0,0,0,0}));
 vr4 = vpa(subs(r4, {d1,d2,d3,d4,d5}, {0,0,0,0,0}));
 vr5 = vpa(subs(r5, {d1,d2,d3,d4,d5}, {0,0,0,0,0}));
+vr5d = vpa(subs(r5d, {d1,d2,d3,d4,d5}, {0,0,0,0,0}));
+vr6 = vpa(subs(r6, {d1,d2,d3,d4,d5}, {0,0,0,0,0}));
 
-vr = round([vr1 vr2 vr3 vr4 vr5], 3)
+vr = round([vr1 vr2 vr3 vr4 vr5 vr6], 3)
--- misc/calc_jacobian.m
+++ misc/calc_jacobian.m
@@ -1,9 +1,9 @@
 clear all;
 syms d1 d2 d3 d4 d5 d6
 
-pos_x = 205*cos(d1)*cos(d2 + pi/2) + 291*cos(d1)*cos(d3)*cos(d2 + pi/2) - 291*cos(d1)*sin(d3)*sin(d2 + pi/2);
-pos_y = 205*cos(d2 + pi/2)*sin(d1) + 291*cos(d3)*cos(d2 + pi/2)*sin(d1) - 291*sin(d1)*sin(d3)*sin(d2 + pi/2);
-pos_z = 205*sin(d2 + pi/2) + 291*cos(d3)*sin(d2 + pi/2) + 291*cos(d2 + pi/2)*sin(d3) + 283;
+pos_x = 104*cos(d5)*(cos(d1)*cos(d3)*cos(d2 + pi/2) - cos(d1)*sin(d3)*sin(d2 + pi/2)) + 112*sin(d5)*(cos(d1)*cos(d3)*cos(d2 + pi/2) - cos(d1)*sin(d3)*sin(d2 + pi/2)) + 112*cos(d5)*(sin(d1)*sin(d4) - cos(d4)*(cos(d1)*cos(d3)*sin(d2 + pi/2) + cos(d1)*cos(d2 + pi/2)*sin(d3))) - 104*sin(d5)*(sin(d1)*sin(d4) - cos(d4)*(cos(d1)*cos(d3)*sin(d2 + pi/2) + cos(d1)*cos(d2 + pi/2)*sin(d3))) + 205*cos(d1)*cos(d2 + pi/2) + 291*cos(d1)*cos(d3)*cos(d2 + pi/2) - 291*cos(d1)*sin(d3)*sin(d2 + pi/2);
+pos_y = 205*cos(d2 + pi/2)*sin(d1) + 104*cos(d5)*(cos(d3)*cos(d2 + pi/2)*sin(d1) - sin(d1)*sin(d3)*sin(d2 + pi/2)) + 112*sin(d5)*(cos(d3)*cos(d2 + pi/2)*sin(d1) - sin(d1)*sin(d3)*sin(d2 + pi/2)) - 112*cos(d5)*(cos(d4)*(cos(d3)*sin(d1)*sin(d2 + pi/2) + cos(d2 + pi/2)*sin(d1)*sin(d3)) + cos(d1)*sin(d4)) + 104*sin(d5)*(cos(d4)*(cos(d3)*sin(d1)*sin(d2 + pi/2) + cos(d2 + pi/2)*sin(d1)*sin(d3)) + cos(d1)*sin(d4)) + 291*cos(d3)*cos(d2 + pi/2)*sin(d1) - 291*sin(d1)*sin(d3)*sin(d2 + pi/2);
+pos_z = 205*sin(d2 + pi/2) + 291*cos(d3)*sin(d2 + pi/2) + 291*cos(d2 + pi/2)*sin(d3) + 104*cos(d5)*(cos(d3)*sin(d2 + pi/2) + cos(d2 + pi/2)*sin(d3)) + 112*sin(d5)*(cos(d3)*sin(d2 + pi/2) + cos(d2 + pi/2)*sin(d3)) - 112*cos(d4)*cos(d5)*(sin(d3)*sin(d2 + pi/2) - cos(d3)*cos(d2 + pi/2)) + 104*cos(d4)*sin(d5)*(sin(d3)*sin(d2 + pi/2) - cos(d3)*cos(d2 + pi/2)) + 283;
 
 
 %%
